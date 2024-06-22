064a62d6357e58e706dd7c2e550f61320faedc0b
Greymagic27 <tolly67p@gmail.com>
2024-06-22 18:32:15 +0100

    Works on 1.21 now

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/module/ServerVersionModule.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/module/ServerVersionModule.java
@@ -14,10 +14,33 @@ public class ServerVersionModule {
 	@Provides
 	@Singleton
 	int provideServerVersion() {
-		// Simple parsing of game version. Might need to be updated in the future depending on how the Minecraft
-		// versions change in the future.
-		String versionIdentifier = Bukkit.getServer().getClass().getPackage().getName().substring(23);
-		return Integer.parseInt(StringUtils.substringBetween(versionIdentifier, "_"));
-	}
+		String packageName = Bukkit.getServer().getClass().getPackage().getName();
 
+		String versionIdentifier = StringUtils.substringBetween(packageName, "v", "_");
+		if (versionIdentifier != null && !versionIdentifier.isEmpty()) {
+			try {
+				return Integer.parseInt(versionIdentifier);
+			} catch (NumberFormatException e) {
+				throw new IllegalArgumentException("Unable to parse version number: " + versionIdentifier, e);
+			}
+		}
+		versionIdentifier = StringUtils.substringAfterLast(packageName, "v");
+		if (versionIdentifier.matches("\\d+_\\d+_R\\d+")) {
+			try {
+				return Integer.parseInt(versionIdentifier.split("_")[0]);
+			} catch (NumberFormatException e) {
+				throw new IllegalArgumentException("Unable to parse version number from vX_XX_RX format: " + versionIdentifier, e);
+			}
+		}
+		if (packageName.equals("org.bukkit.craftbukkit")) {
+			String bukkitVersion = Bukkit.getBukkitVersion();
+			versionIdentifier = StringUtils.substringBefore(bukkitVersion, "-");
+			try {
+				return Integer.parseInt(versionIdentifier.split("\\.")[0]);
+			} catch (NumberFormatException e) {
+				throw new IllegalArgumentException("Unable to parse version number from Bukkit version: " + bukkitVersion, e);
+			}
+		}
+		throw new IllegalArgumentException("Unexpected package name format: " + packageName);
+	}
 }
